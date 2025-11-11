1cbd6667b20443b749a0a838a61f283ebe9b4fa5
Guilherme Neis <guilherme.neis@deltasul.com.br>
2025-11-11 11:13:10 -0300

    Melhora o design da barra lateral

--- js/alterna-paginas.js
+++ js/alterna-paginas.js
@@ -1,21 +1,59 @@
 export function configuraAlternaPaginas() {
+    let navItem1 = document.getElementById("nav-item-1");
+    let navItem2 = document.getElementById("nav-item-2");
+    let navItem3 = document.getElementById("nav-item-3");
+    let navItem4 = document.getElementById("nav-item-4");
+    let sobreMim = document.getElementById("sobre-mim");
+    let formacao = document.getElementById("formacao");
+    let portfolio = document.getElementById("portfolio");
+    let contato = document.getElementById("contato");
 
-    const sobreMim = document.getElementById("sobre-mim");
-    const formacao = document.getElementById("formacao");
-    const portfolio = document.getElementById("portfolio");
-    const contato = document.getElementById("contato");
-    const navMenu = document.getElementById("nav-principal-menu");
+    navItem1.style.backgroundColor = "#f1f1f1ff";
+    navItem2.style.backgroundColor = "#ffffff00";
+    navItem3.style.backgroundColor = "#ffffff00";
+    navItem4.style.backgroundColor = "#ffffff00";
 
-    sobreMim.checked = true;
-    sobreMim.hidden = false;
     formacao.hidden = true;
     portfolio.hidden = true;
     contato.hidden = true;
 
-    navMenu.addEventListener("click", (event) => {
-        sobreMim.hidden = event.target.id !== 'nav-item-1'
-        formacao.hidden = event.target.id !== 'nav-item-2'
-        portfolio.hidden = event.target.id !== 'nav-item-3'
-        contato.hidden = event.target.id !== 'nav-item-4'
+    document.getElementById("nav-principal-menu")
+        .addEventListener("click", (event) => {
+        if (event.target.id == 'nav-principal-menu') {
+            return;
+        }
+        sobreMim.hidden = event.target.id !== 'nav-item-1';
+        formacao.hidden = event.target.id !== 'nav-item-2';
+        portfolio.hidden = event.target.id !== 'nav-item-3';
+        contato.hidden = event.target.id !== 'nav-item-4';
+        console.log(event.target.id);
+        switch (event.target.id) {
+            case 'nav-item-1':
+                navItem1.style.backgroundColor = "#f1f1f1ff";
+                navItem2.style.backgroundColor = "#ffffff00";
+                navItem3.style.backgroundColor = "#ffffff00";
+                navItem4.style.backgroundColor = "#ffffff00";
+                break;
+            case 'nav-item-2':
+                navItem1.style.backgroundColor = "#ffffff00";
+                navItem2.style.backgroundColor = "#f1f1f1ff";
+                navItem3.style.backgroundColor = "#ffffff00";
+                navItem4.style.backgroundColor = "#ffffff00";
+                break;
+            case 'nav-item-3':
+                navItem1.style.backgroundColor = "#ffffff00";
+                navItem2.style.backgroundColor = "#ffffff00";
+                navItem3.style.backgroundColor = "#f1f1f1ff";
+                navItem4.style.backgroundColor = "#ffffff00";
+                break;
+            case 'nav-item-4':
+                navItem1.style.backgroundColor = "#ffffff00";
+                navItem2.style.backgroundColor = "#ffffff00";
+                navItem3.style.backgroundColor = "#ffffff00";
+                navItem4.style.backgroundColor = "#f1f1f1ff";
+                break;
+            default:
+                break;
+        }
     });
 }
